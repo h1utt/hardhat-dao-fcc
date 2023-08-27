7d84bcfdc062e1ab74ab99fd9327544e6c99f1f0
veoquynhs <hieu.tt2304@gmail.com>
2023-08-27 23:52:38 +0700

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,82 @@
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
+import "dotenv/config"
+import "solidity-coverage"
+import "hardhat-deploy"
+import { HardhatUserConfig } from "hardhat/config"
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        localhost: {
+            chainId: 31337,
+            blockConfirmations: 1,
+            allowUnlimitedContractSize: true,
+        },
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+            allowUnlimitedContractSize: true,
+        },
+        sepolia: {
+            chainId: 11155111,
+            blockConfirmations: 6,
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            saveDeployments: true,
+        },
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+        },
+        customChains: [
+            {
+                network: "sepolia",
+                chainId: 11155111,
+                urls: {
+                    apiURL: "https://api-sepolia.etherscan.io/api",
+                    browserURL: "https://sepolia.etherscan.io/",
+                },
+            },
+        ],
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8",
+            },
+            {
+                version: "0.8.0",
+            },
+        ],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 10000000, // 10000 secs max
+    },
+}
